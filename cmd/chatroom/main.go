0c6816782ed081806faa54967a26fcef061de9d3
mrgulugulu <492238938@qq.com>
2022-03-12 17:36:22 +0800

    修改了模板

--- cmd/chatroom/main.go
+++ cmd/chatroom/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"wsChatroom/global"
 	"wsChatroom/server"
 )
 
@@ -21,9 +22,9 @@ Go语言编程之旅 —— 一起用Go做项目：ChatRoom，start on：%s
 )
 
 // 在main运行前会运行一次
-// func init() {
-// 	global.Init()
-// }
+func init() {
+	global.Init()
+}
 
 func main() {
 	fmt.Printf(banner+"\n", addr)
